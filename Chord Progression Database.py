fbf79a6b9d904cb4967cf3b33d6f7efcc88c7a98
Miles <mileshiggins7@gmail.com>
2021-07-18 17:48:06 +1000

    Added Chord and Progression classes

--- Chord Progression Database.py
+++ Chord Progression Database.py
@@ -1,7 +1,20 @@
-# add to git so I can start making commits
 import music21
 import os
 from music21 import *
+from anytree import Node, RenderTree
+
+
+class Chord:
+    def __init__(self, name, progression):
+        self.chord_name = name  # music 21 pitched common name of chord
+        self.progression = progression  # progression object which stores filterable details
+
+
+class Progression:
+    def __init__(self, genre, tonality, song_section=None):
+        self.song_section = song_section
+        self.tonality = tonality
+        self.genre = genre
 
 
 def build_progression_tree(root_directory):
@@ -10,11 +23,16 @@ def build_progression_tree(root_directory):
     # these subfolders have subfolders sorted by song section (exceptnot all do)
     # these subfolders have subfolders sorted by major or minor (except not all do)
     # these subfolders have subfolders have subfolders sorted by key
-    for root, dirs, files in os.walk(root_directory, topdown=False):
+    for root, dirs, files in os.walk(root_directory, topdown=True):
         for name in files:
             print(os.path.join(root, name))
         for name in dirs:
             print(os.path.join(root, name))
+
+        # if midi file, open
+            # if first chord isn't a pre-exiting tree, create it
+            # if subsequent chord isn't in tree yet, add to tree
+            # if reach end of progression, add tags
 
     return
 
@@ -39,5 +57,5 @@ def find_chord_progression(midi_filename):
     return progression
 
 
-# print(find_chord_progression('sample 1126.mid'))
-print(build_progression_tree('Compressed Chord Templates'))
+print(find_chord_progression('sample 1126.mid'))
+# print(build_progression_tree('Compressed Chord Templates'))
